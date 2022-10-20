dec9c3b5f1ae249bd08b74c986e4f0efa4ea5fcc
Obriel Zvoushe <obrielzvoushe1@gmail.com>
2022-10-20 11:33:18 +0200

    added router

--- react-dashboard/src/components/NavbarComp.js
+++ react-dashboard/src/components/NavbarComp.js
@@ -2,21 +2,29 @@ import React, { Component } from 'react'
 import { Navbar,Nav,NavDropdown,Form,FormControl,Button } from 'react-bootstrap'
 
 import Container from 'react-bootstrap/Container';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
 
 export default class NavbarComp extends Component{
     render(){
         return(
+            <Router>
             <div>
                 <Navbar bg="dark" variant="dark">
         <Container>
-                <Navbar.Brand href="#dashboard">Dashboard</Navbar.Brand>
+                <Navbar.Brand as={Link} to={"/dashboard"}>Dashboard</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#page1">Page 1</Nav.Link>
-            <Nav.Link href="#page2">Page 2</Nav.Link>
+            <Nav.Link as={Link} to={"/page1"}>Page 1</Nav.Link>
+            <Nav.Link as={Link} to={"/page2"}>Page 2</Nav.Link>
           </Nav>
           </Container>
           </Navbar>
             </div>
+            </Router>
         )
     }
 }
