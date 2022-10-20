0600cdb240c2f81c494b09bb1a6fa513497c3c82
Obriel Zvoushe <obrielzvoushe1@gmail.com>
2022-10-20 21:39:54 +0200

    added items to an array

--- react-dashboard/src/components/Page1.js
+++ react-dashboard/src/components/Page1.js
@@ -1,11 +1,32 @@
-import React, {  Component} from 'react'
+import React, {  Component, useState} from 'react'
+import "./Page1.css";
 
-export default class Page1 extends Component {
-    render() {
+export default function Page1() {
+    
+
+const [appState,changeState] = useState({
+    objects: [{ id: 1, toggled: false },
+        { id: 2, toggled: false },
+        { id: 3, toggled: false },
+        { id: 4, toggled: false }
+    ]
+});
+
+function toggleActiveStyles(index){
+    if (appState.objects[index].toggled){
+        return "box active";
+    } else{
+        return "box inactive";
+    }
+}
+
         return(
-            <div>
+            <div className="Page1">
                 <h1>Page 1</h1>
+                {appState.objects.map((element, index) => (
+                    <div key={index} className={toggleActiveStyles(index)}></div>
+                ))}
             </div>
         )
-    }
+    
 }
